0508a60de1c8aa1e9938f70494881eef093d1ce9
Adel Aref <aaref@ipmagix.com>
2020-01-01 13:49:18 +0200

    Initial Commit

---
+++ CairoICT2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CairoICT2020
+//
+//  Created by User on 1/1/20.
+//  Copyright © 2020 Adel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
